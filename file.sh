25ad7e51625479b30ce5d3ce7ac29efeed1d480f
meeravali7353 <meeravali7353@gmail.com>
2022-07-17 22:26:31 +0530

    added repo1

---
+++ file.sh
@@ -0,0 +1,3 @@
+hi linux is a os 
+and this is use for security purpose
+i am learing devops by using linux commands[A[A[A[B[B[C[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[D[3~[3~[3~[3~[3~[3~[3~[3~[A[B[B
